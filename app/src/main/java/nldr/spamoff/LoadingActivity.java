41938dc91da3e22758a5077f1bdf00d2cde8b12d
Roee Zolantz <fadida_is_gay_and_proud@lol.com>
2016-11-08 21:49:18 +0200

    Added loading screen with timeout of 2 seconds and changed the primary color

---
+++ app/src/main/java/nldr/spamoff/LoadingActivity.java
@@ -0,0 +1,33 @@
+package nldr.spamoff;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        final Context context = this;
+
+        Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.postDelayed(runnable, 2200);
+        handler.postDelayed(runnable, 2200);
+    }
+}
