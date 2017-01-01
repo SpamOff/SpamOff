4dadbcdcb33e13fc92f05207dafa52e5cf86321f
Roee Zolantz <xdlunatic@gmail.com>
2017-01-01 18:14:25 +0200

    Made somr fixed in ShareActivity

--- app/src/main/java/nldr/spamoff/share.java
+++ app/src/main/java/nldr/spamoff/share.java
@@ -1,7 +1,10 @@
 package nldr.spamoff;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class share extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class share extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home)
+            finish();
+
+        return true;
     }
 }
