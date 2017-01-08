6d68b615cece9e8cf792c31ba6c8e0d22e3b4386
Roee Zolantz <xdlunatic@gmail.com>
2017-01-08 18:55:48 +0200

    Added instance verifier service and updated the manifest

---
+++ app/src/main/java/nldr/spamoff/fbsInstanceIdService.java
@@ -0,0 +1,27 @@
+package nldr.spamoff;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class fbsInstanceIdService extends FirebaseInstanceIdService {
+
+    private final String TAG = "fbsInstanceIdService";
+
+    public fbsInstanceIdService() {
+    }
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
--- app/src/main/java/nldr/spamoff/fbsMessagingService.java
+++ app/src/main/java/nldr/spamoff/fbsMessagingService.java
@@ -3,10 +3,29 @@ package nldr.spamoff;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class fbsMessagingService extends FirebaseMessagingService {
+
+    private final String TAG = "fbsMessagingService";
+
     public fbsMessagingService() {
     }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+    }
+
+
+
 }
