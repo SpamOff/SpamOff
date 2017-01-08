bc80e1913095d892b5c7d62276580525672f0dc0
Roee Zolantz <xdlunatic@gmail.com>
2017-01-08 18:26:49 +0200

    Added firebase messagin background service

---
+++ app/src/main/java/nldr/spamoff/fbsMessagingService.java
@@ -0,0 +1,12 @@
+package nldr.spamoff;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class fbsMessagingService extends FirebaseMessagingService {
+    public fbsMessagingService() {
+    }
+}
