a9c9728e75569d8c2ed365d344dee85d663e55d1
Roee Zolantz <xdlunatic@gmail.com>
2017-03-26 23:30:21 +0300

    Added Crashlytics to the logger

--- app/src/main/java/nldr/spamoff/Logger.java
+++ app/src/main/java/nldr/spamoff/Logger.java
@@ -1,8 +1,8 @@
 package nldr.spamoff;
 
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.answers.Answers;
-import com.crashlytics.android.answers.CustomEvent;
+
+import nldr.spamoff.AndroidStorageIO.CookiesHandler;
 
 /**
  * Created by Roee on 09/01/2017.
@@ -10,12 +10,13 @@ import com.crashlytics.android.answers.CustomEvent;
 
 public class Logger {
     public static Boolean writeToLog(Exception ex) {
-        if (Crashlytics.getInstance() != null) {
+        //ex.printStackTrace();
+
+        if (CookiesHandler.getCrashlyticsStatus() && Crashlytics.getInstance() != null) {
             Crashlytics.logException(ex);
             return true;
         }
-//        if (Answers.getInstance() != null) {
-//            Answers.getInstance().logCustom(new CustomEvent("Exception").putCustomAttribute("message", ex.getMessage()));
-        else return false;
+        
+        return false;
     }
 }
